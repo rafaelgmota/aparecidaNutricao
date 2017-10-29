1fba2b3cfd95551c8fc4377f9b47c588d907c511
Rafael Mota <faelmota@gmail.com>
2017-10-29 14:56:23 -0300

    calculando imc para todas as linhas

--- js/principal.js
+++ js/principal.js
@@ -1,29 +1,33 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
+var atualizaImc = function(paciente) {
+	var peso = paciente.querySelector(".info-peso").textContent;
+	var altura = paciente.querySelector(".info-altura").textContent;
 
-tdImc = paciente.querySelector(".info-imc");
+	tdImc = paciente.querySelector(".info-imc");
 
-var pesoValido = true;
-var alturaValida = true;
+	var pesoValido = true;
+	var alturaValida = true;
 
-if(peso <= 0 || peso > 1000) {
-	console.log("Peso Inválido");
-	tdImc.textContent = "Peso Inválido";
-	pesoValido = false;
+	if(peso <= 0 || peso > 1000) {
+		console.log("Peso Inválido");
+		tdImc.textContent = "Peso Inválido";
+		pesoValido = false;
+	}
+
+	if(altura <= 0 || altura >= 3.00) {
+		console.log("Altura Inválida");
+		tdImc.textContent = "Altura Inválida";
+		alturaValida = false;
+	}
+
+	if(pesoValido && alturaValida) {
+		var imc = peso/(altura*altura);
+		tdImc.textContent = imc.toFixed(2);
+	}
 }
 
-if(altura <= 0 || altura >= 3.00) {
-	console.log("Altura Inválida");
-	tdImc.textContent = "Altura Inválida";
-	alturaValida = false;
-}
-
-if(pesoValido && alturaValida) {
-	var imc = peso/(altura*altura);
-	tdImc.textContent = imc;
-}
+pacientes.forEach(atualizaImc);
