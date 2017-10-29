24865eb4b86fec2a38063aab84b3ad24bc1b95ae
Rafael Mota <faelmota@gmail.com>
2017-10-29 14:32:43 -0300

    Validando altura e peso

--- js/principal.js
+++ js/principal.js
@@ -6,6 +6,24 @@ var paciente = document.querySelector("#primeiro-paciente");
 var peso = paciente.querySelector(".info-peso").textContent;
 var altura = paciente.querySelector(".info-altura").textContent;
 
-var imc = peso/(altura*altura);
+tdImc = paciente.querySelector(".info-imc");
 
-paciente.querySelector(".info-imc").textContent = imc;
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso > 1000) {
+	console.log("Peso Inválido");
+	tdImc.textContent = "Peso Inválido";
+	pesoValido = false;
+}
+
+if(altura <= 0 || altura >= 3.00) {
+	console.log("Altura Inválida");
+	tdImc.textContent = "Altura Inválida";
+	alturaValida = false;
+}
+
+if(pesoValido && alturaValida) {
+	var imc = peso/(altura*altura);
+	tdImc.textContent = imc;
+}
