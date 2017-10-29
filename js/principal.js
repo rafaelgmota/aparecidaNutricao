9ea44254697ebc5088ef4bb3044ff789af5ead9a
Rafael Mota <faelmota@gmail.com>
2017-10-29 14:27:52 -0300

    calculando o imc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,11 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var imc = peso/(altura*altura);
+
+paciente.querySelector(".info-imc").textContent = imc;
