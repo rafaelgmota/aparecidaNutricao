06a24a64c4357edc270d0fcd5cb387cd0d1221f0
Rafael Mota <faelmota@gmail.com>
2017-11-01 17:00:43 -0300

    Adicionando pacientes a tabela

--- js/principal.js
+++ js/principal.js
@@ -33,3 +33,38 @@ var atualizaImc = function(paciente) {
 }
 
 pacientes.forEach(atualizaImc);
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+	event.preventDefault();
+
+	var form = document.querySelector('#form-adiciona');
+
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+
+	var pacienteTr = document.createElement("tr");
+
+	var nomeTd = document.createElement("td");
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+
+	nomeTd.textContent = nome;
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+
+	var tableBody = document.querySelector("#tabela-pacientes");
+
+	tableBody.appendChild(pacienteTr);
+
+});
